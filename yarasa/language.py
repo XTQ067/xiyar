2307104cc9f33d6d205e228719e942856678650b
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-14 14:24:15 +0400

    Update language.py

--- yarasa/language.py
+++ yarasa/language.py
@@ -28,4 +28,4 @@ def importlang ():
     return COUNTRY, LANGUAGE, TZ
 
 COUNTRY, LANGUAGE, TZ = importlang()
-LANG = loads(open(f"./asena_installer/language/{LANGUAGE}.asenajson", "r").read())["STRINGS"]
+LANG = loads(open(f"./yarasa/language/{LANGUAGE}.yarasajson", "r").read())["STRINGS"]
