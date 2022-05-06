ced47ba70d95d925e1021115df7a2d40a4f10ebe
Nusret <89344065+nusrte@users.noreply.github.com>
2022-05-06 18:57:36 +0400

    Update __main__.py

--- yarasa/__main__.py
+++ yarasa/__main__.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     config['CLEAN_WELCOME'] = "True"
     config['CONSOLE_LOGGER_VERBOSE'] = "False"
     config['COUNTRY'] = COUNTRY
-    config['DEFAULT_BIO'] = "@YarasaBots"
+    config['DEFAULT_BIO'] = "@NeonUserBot"
     config['GALERI_SURE'] = "60"
     config['CHROME_DRIVER'] = "/usr/sbin/chromedriver"
     config['GOOGLE_CHROME_BIN'] = "/usr/sbin/chromium"
