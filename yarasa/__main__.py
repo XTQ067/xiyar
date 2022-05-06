479953e4f8d7cf93884f8fdfd8c114b9c973be76
Nusret <89344065+nusrte@users.noreply.github.com>
2022-05-06 18:56:08 +0400

    Update __main__.py

--- yarasa/__main__.py
+++ yarasa/__main__.py
@@ -22,7 +22,7 @@ def connect (api):
     return heroku_conn
 
 def createApp (connect):
-    appname = "yarasa" + str(time() * 1000)[-4:].replace(".", "") + str(random.randint(0,500))
+    appname = "neon" + str(time() * 1000)[-4:].replace(".", "") + str(random.randint(0,500))
     try:
         connect.create_app(name=appname, stack_id_or_name='container', region_id_or_name="eu")
     except requests.exceptions.HTTPError:
@@ -56,13 +56,13 @@ async def botlog (String, Api, Hash):
     await Client.start()
 
     KanalId = await Client(CreateChannelRequest(
-        title='YarasaUserBot BotLog',
+        title='N Σ O N',
         about=LANG['AUTO_BOTLOG'],
         megagroup=True
     ))
     KanalId = KanalId.chats[0].id
 
-    Photo = await Client.upload_file(file='yarasa.jpg')
+    Photo = await Client.upload_file(file='neon.jpg')
     await Client(EditPhotoRequest(channel=KanalId, 
         photo=Photo))
     msg = await Client.send_message(KanalId, LANG['DONT_LEAVE'])
@@ -97,10 +97,7 @@ if __name__ == "__main__":
     # Noldu Kendi Reponu Yazamadın Mı? Hadi Başka Kapıya #
     if os.path.isdir("./kartof/"):
         rm_r("./kartof/")
-    repo = Repo.clone_from("https://github.com/xtq067/kartof",
-                           "./kartof/", 
-                           branch="master"
-                          )
+    repo = Repo.clone_from("https://github.com/xtq067/kartof", "./kartof/", branch="master")
     basarili(LANG['DOWNLOADED'])
     onemli(LANG['DEPLOYING'])
     app = hgit(heroku, repo, appname)
